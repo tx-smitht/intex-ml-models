f0f324a25ab31af2977844ac2bff118447671167
tx-smitht <tx.smiththomas@gmail.com>
2023-04-11 18:54:04 -0600

    support for multiple models

--- app.py
+++ app.py
@@ -21,10 +21,23 @@ async def read_root():
     return {"message": "Welcome to the API!"}
 
 # Define endpoint for making predictions
-@app.post('/predict')
+@app.post('/predict-head-direction')
 def predict(data:dict):
   # Load model from .pkl file
-  with open('./model.pkl','rb') as file:
+  with open('./headdirection_model.pkl','rb') as file:
+    model = pickle.load(file)
+    # Convert input data to DataFrame
+    df = pd.DataFrame(data, index=[0])
+    # Make prediction
+    prediction = model.predict(df)
+    # Return Prediction as JSON response
+    return {'prediction': prediction[0]}
+    #df[['wrapping','depth','adultsubadult','facebundles','preservation','length']]
+
+@app.post('/predict-wrapping')
+def predict(data:dict):
+  # Load model from .pkl file
+  with open('./wrapping_model.pkl','rb') as file:
     model = pickle.load(file)
     # Convert input data to DataFrame
     df = pd.DataFrame(data, index=[0])
